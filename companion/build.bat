f0a26ea9ce5c4a6e838bc84abdf5154d094c8500
Balazs Gyurak <ba32107@gmail.com>
2020-04-18 10:54:19 +0100

    Implement windowhandler_windows.cpp

--- companion/build.bat
+++ companion/build.bat
@@ -2,4 +2,4 @@
 
 if exist bin ( rmdir /S/Q bin )
 mkdir bin
-g++ -o bin/tabfloater_companion.exe src/windowhandler_windows.cpp src/tabfloater_companion.cpp
+g++ -o bin/tabfloater_companion.exe src/windowhandler_windows.cpp src/tabfloater_companion.cpp -lole32
--- companion/src/windowhandler_windows.cpp
+++ companion/src/windowhandler_windows.cpp
@@ -1,8 +1,28 @@
 #include "windowhandler.h"
-#include <fstream>
+#include <shobjidl.h>
+
+const GUID CLSID_TaskbarList = {0x56FDF344, 0xFD6D, 0x11D0, {0x95, 0x8A, 0x00, 0x60, 0x97, 0xC9, 0xA0, 0x90}};
+const GUID IID_ITaskbarList = {0x56FDF342, 0xFD6D, 0x11D0, {0x95, 0x8A, 0x00, 0x60, 0x97, 0xC9, 0xA0, 0x90}};
 
 void setWindowAlwaysOnTopAndSkipTaskbar(std::string windowName) {
-    std::ofstream out("c:\\work\\out.txt");
-    out << windowName;
-    out.close();
+    HWND windowHandler = FindWindowA(NULL, windowName.c_str());
+
+    SetWindowPos(windowHandler, HWND_TOPMOST, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE);
+
+    CoInitialize(NULL);
+
+    ITaskbarList* pTaskbarList = NULL;
+    if(FAILED(CoCreateInstance(CLSID_TaskbarList, NULL, CLSCTX_ALL, IID_ITaskbarList, (void **)&pTaskbarList))){
+        // error
+    } else {
+        if (SUCCEEDED(pTaskbarList->HrInit())) {
+            pTaskbarList->DeleteTab(windowHandler);
+        } else {
+            //log warn
+        }
+
+        pTaskbarList->Release();
+    }
+
+    CoUninitialize();
 }
