9f29fe9a96ffabb10fe41463e64ef6f33d0f7673
Balazs Gyurak <ba32107@gmail.com>
2020-04-20 19:07:32 +0100

    Remove obsolete build.sh

--- companion/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-rm -rf ./bin
-mkdir ./bin
-g++ ./src/windowhandler_linux.cpp ./libs/loguru/loguru.cpp ./src/tabfloater_companion.cpp -I src -o ./bin/tabfloater_companion -lX11 -std=c++11 -lpthread -ldl
