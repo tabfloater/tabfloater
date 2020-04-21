2d65262413d9ca70f068c1911db583f1b37dfd62
Balazs Gyurak <ba32107@gmail.com>
2020-04-21 21:30:08 +0100

    Fix CMake for windows

--- companion/build.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-if exist bin ( rmdir /S/Q bin )
-mkdir bin
-g++ -static -o bin/tabfloater_companion.exe libs/loguru/src/loguru.cpp src/windowhandler_windows.cpp src/tabfloater_companion.cpp -lole32
