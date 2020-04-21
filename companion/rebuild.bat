24109fa90c13e3b76213d24f3e3022ca577382e0
Balazs Gyurak <ba32107@gmail.com>
2020-04-21 21:32:45 +0100

    Remove unnecessary line

--- companion/rebuild.bat
+++ companion/rebuild.bat
@@ -3,6 +3,5 @@
 if exist build ( rmdir /S/Q build )
 mkdir build && cd build
 
-rem g++ -static -o bin/tabfloater_companion.exe libs/loguru/src/loguru.cpp src/windowhandler_windows.cpp src/tabfloater_companion.cpp -lole32
 cmake -G "MinGW Makefiles" ..
 mingw32-make
