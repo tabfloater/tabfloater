59f6e04c68f4ce6edf72f54de0b695054aa7c0be
Balazs Gyurak <ba32107@gmail.com>
2020-04-26 16:53:46 +0100

    Focus parent window when floating tab is focused

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -28,6 +28,14 @@ chrome.windows.onRemoved.addListener(function (closingWindowId) {
   });
 });
 
+chrome.windows.onFocusChanged.addListener(function (windowId) {
+  tryGetFloatingTab(function (floatingTab, floatingTabProperties) {
+    if (floatingTab && floatingTab.windowId === windowId) {
+      chrome.windows.update(floatingTabProperties.parentWindowId, { focused: true });
+    }
+  });
+});
+
 chrome.commands.onCommand.addListener(function (command) {
   console.log("comm rec: " + command);
   tryGetFloatingTab(function (floatingTab, tabProps) {
