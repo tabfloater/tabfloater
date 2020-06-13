659aa3eee844d6c9de3774d955f6f091ce260d1a
Balazs Gyurak <ba32107@gmail.com>
2020-06-13 19:07:39 +0100

    Fix gitattributes

--- extension/postinstall.bat
+++ extension/postinstall.bat
@@ -6,4 +6,4 @@ copy node_modules\\webextension-polyfill\\dist\\*.min.js* src\\libs\\webextensio
 mkdir src\\libs\\uikit
 copy node_modules\\uikit\\dist\\js\\*.min.js src\\libs\\uikit
 
-sass src\\sass\\tabfloater.scss > src\\css\\uk-tabfloater.css
+sass src\\sass\\tabfloater.scss > src\\css\\generated\\uk-tabfloater.css
