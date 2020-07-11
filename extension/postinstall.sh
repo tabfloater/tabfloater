8336694140b3ef27bbdb165cc199fd0ef7391223
Balazs Gyurak <ba32107@gmail.com>
2020-07-11 13:27:42 +0100

    Test commit7

--- extension/postinstall.sh
+++ extension/postinstall.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 mkdir src/libs/webextension-polyfill
-cp node_modules/webextension-polyfill/dist/*.min.js* src/libs/webextension-polyfill
+#cp node_modules/webextension-polyfill/dist/*.min.js* src/libs/webextension-polyfill
 
 mkdir src/libs/uikit
 cp node_modules/uikit/dist/js/*.min.js src/libs/uikit
 
-#sass src/sass/tabfloater.scss > src/css/generated/uk-tabfloater.css
+sass src/sass/tabfloater.scss > src/css/generated/uk-tabfloater.css
