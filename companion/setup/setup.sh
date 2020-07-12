1abf69ad3fa06579c5e5817181af756cec7fdd84
Balazs Gyurak <ba32107@gmail.com>
2020-07-12 16:13:47 +0100

    Remove unused setup directory

--- companion/setup/setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-SETUP_DIR=`dirname $0 | xargs realpath`
-COMPANION_EXECUTABLE=`realpath "$SETUP_DIR/../build/tabfloater_companion"`
-
-sed "s|COMPANION_EXECUTABLE_PLACEHOLDER|$COMPANION_EXECUTABLE|g" io.github.tabfloater.companion_SAMPLE.json > io.github.tabfloater.companion.json
