de89e5bcc8db92ad0ef4c9fc69594beb0540001d
Balazs Gyurak <ba32107@gmail.com>
2020-10-09 07:45:09 +0100

    Create appimage creation script

--- companion/build.sh
+++ companion/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 function clean() {
     cmake --build build --target clean_manifests
     rm -rf build
@@ -14,15 +16,14 @@ _MODE=$1
 
 if [[ "$_MODE" == "-h" || "$_MODE" == "--help" ]]; then
     echo
-    echo "Builds/packages TabFloater Companion and installs native messaging host manifests."
+    echo "Builds TabFloater Companion and installs native messaging host manifests."
     echo
     echo "When invoked without arguments, the script performs a full build and manifest installation if no build"
     echo "directory exists. A normal build is performed otherwise, without manifest installation."
     echo
-    echo "Usage: $0 [-c | --clean] [-p | --package] [-r | --rebuild]"
+    echo "Usage: $0 [-c | --clean] [-r | --rebuild]"
     echo
-    echo "  -c  --clean      Deletes the build directory and removes the manifests. The dist directory is kept."
-    echo "  -p  --package    Packages the companion into the dist directory. The directory is cleaned first."
+    echo "  -c  --clean      Deletes the build directory and removes the manifests."
     echo "  -r  --rebuild    Performs a full rebuild. Same as '--clean', then performing a build."
     echo
     exit
@@ -32,15 +33,6 @@ if [[ "$_MODE" == "-c" || "$_MODE" == "--clean" ]]; then
     if [ -e "build" ]; then
         clean
     fi
-    exit
-fi
-
-if [[ "$_MODE" == "-p" || "$_MODE" == "--package" ]]; then
-    if [ -e "dist" ]; then
-        rm -rf dist
-    fi
-    cmake -S . -B dist
-    cmake --build dist --target tabfloater-companion package_source #generate_appimage
     exit
 fi
 
---
+++ companion/packaging/linux/create_appimage.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+set -e
+
+_COMPANION_DIR=$(git rev-parse --show-toplevel)/companion
+_APPIMAGE_RESOURCES_DIR=$_COMPANION_DIR/packaging/linux/appimage
+_DESKTOP_FILE=$_APPIMAGE_RESOURCES_DIR/tabfloater-companion.desktop
+_ICON_FILE=$_APPIMAGE_RESOURCES_DIR/tabfloater-companion.svg
+_BUILD_DIR=$_COMPANION_DIR/dist
+
+cd $_COMPANION_DIR
+
+if [ -e $_BUILD_DIR ]; then
+    echo "Cleaning existing build directory..."
+    rm -rf $_BUILD_DIR
+fi
+
+cmake -S . -B $_BUILD_DIR
+cmake --build $_BUILD_DIR
+
+_EXECUTABLE_NAME=$(cmake -L -N dist | grep MAIN_TARGET | cut -f 2 -d "=")
+_VERSION=$(cmake -L -N dist | grep VERSION | cut -f 2 -d "=")
+
+echo
+echo "Package name: $_EXECUTABLE_NAME"
+echo "Version: $_VERSION"
+echo
+
+cd $_BUILD_DIR
+
+echo "Downloading linuxdeploy..."
+wget --no-verbose --show-progress https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage
+trap "rm ./linuxdeploy-x86_64.AppImage" EXIT
+
+chmod +x ./linuxdeploy-x86_64.AppImage
+echo "Generating AppImage..."
+export VERSION=$_VERSION && ./linuxdeploy-x86_64.AppImage --appdir AppDir --executable ./$_EXECUTABLE_NAME --desktop-file $_DESKTOP_FILE --icon-file $_ICON_FILE --output appimage
+
+if [[ $? -eq 0 ]]; then
+    _APPIMAGE_FILENAME=$_BUILD_DIR/$(ls tabfloater-companion*.AppImage)
+    echo
+    echo "Successfully built:"
+    echo $_APPIMAGE_FILENAME
+fi
