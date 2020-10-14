8a43dbe5ea9230aafd6958bc3018e51287659230
Balazs Gyurak <ba32107@gmail.com>
2020-10-14 22:10:02 +0100

    Rework Linux packaging & add PPA (#154)
    * Change all URLs to not use www
    
    * WIP
    
    * PPA WIP
    
    * WIP
    
    * Create appimage creation script
    
    * Rename appimage script
    
    * Add dist_ppa to gitignore
    
    * Move ppa files
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP

---
+++ companion/packaging/linux/add_changelog.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+export DEBFULLNAME="Balazs Gyurak"
+export DEBEMAIL="balazs@tabfloater.io"
+
+_COMPANION_DIR=$(git rev-parse --show-toplevel)/companion
+
+cd $_COMPANION_DIR/packaging/linux/ubuntu-ppa
+rm -f debian/changelog.dch
+
+dch --distribution=SERIES --force-distribution --local "~SERIES"
