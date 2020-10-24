aa8b654bf852d0396aa91abd7687175812ccf85f
Balazs Gyurak <ba32107@gmail.com>
2020-10-24 20:23:41 +0100

    Bump to 0.8.0

--- extension/src/js/constants.js
+++ extension/src/js/constants.js
@@ -16,8 +16,8 @@
 
 export const CompanionName = "io.github.tabfloater.companion";
 export const CompanionLatestVersions = {
-    Linux: "0.7.0",
-    Windows: "0.7.0"
+    Linux: "0.8.0",
+    Windows: "0.8.0"
 };
 
 export const CommandToPositionMapping = {
